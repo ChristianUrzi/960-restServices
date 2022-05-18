aa676975aa0d1b37cc467c2d6f69e16f22d6114b
ChristianUrzi <urzi.christian@iisgalvanimi.edu.it>
2022-05-18 19:13:23 +0000

    commentato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';//importa il modulo
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,21 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //dichiarazione delle variabili e inizializzazione con virgolette vuote
   nome:string = ""
   naz:string = ""
 
+  //lo usiamo per creare un oggetto di tipo HTTpclient
   constructor(private richiesta:HttpClient){}
   public mostra(){
+    //inviamo la richiesta get all'Api e quando finisce eseguiamo il subscribe, eseguito in ogni caso
     this.richiesta.get("https://api.nationalize.io/?name="+this.nome).subscribe(
+      //se esito positivo, datinaz contiene l'output dell'Api
       datinaz => {
+        //assegnamo alla variabile naz il dato che prendiamo da datinaz
         this.naz= datinaz["country"][0]["country_id"]
-        console.log(datinaz)
+        
       },
+      //se esito negativo, errore conterrà il problema riscontrato nella richiesta get
       errore => {
         console.log(errore["error"])
       }
     )
-
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';//importo modulo
+import {HttpClientModule} from '@angular/common/http'//importo modulo
 import { AppComponent } from './app.component';
 
 @NgModule({
